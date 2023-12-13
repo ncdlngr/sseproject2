d151597f58cf933c740964c9c5e85514ee9cce0c
Nico <nico.dollinger94@gmail.com>
2023-12-14 00:02:52 +0100

    Add start test button and navbar to edit-test.ejs

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import express from "express";
 import bcrypt from "bcrypt";
 import db from "./database.js";
 import session from "express-session";
-import 'dotenv/config';
+import "dotenv/config";
 import e from "express";
-import ISO6391 from 'iso-639-1';
+import ISO6391 from "iso-639-1";
 import languageCountryMapping from "./languagemapping.js";
 
 const app = express();
@@ -19,13 +19,12 @@ app.use(
     cookie: { secure: false }, // Set to true if using https
   })
 );
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
-
 
 function getCountryCodeForLanguage(languageCode) {
   // Basic mapping of language codes to country codes
-  return languageCountryMapping[languageCode] || 'default'; // Use a default value if no mapping found
+  return languageCountryMapping[languageCode] || "default"; // Use a default value if no mapping found
 }
 
 function getLanguageDetails(languageCode) {
@@ -36,21 +35,22 @@ function getLanguageDetails(languageCode) {
 
 // Routes
 
-app.get('/', (req, res) => {
-    if (req.session.userId) {
-      res.redirect('/dashboard');
-    } else {
-        res.redirect('/login');
-    }});
+app.get("/", (req, res) => {
+  if (req.session.userId) {
+    res.redirect("/dashboard");
+  } else {
+    res.redirect("/login");
+  }
+});
 
-app.get('/dashboard', (req, res) => {
-    if (req.session.userId) {
-      res.render('dashboard');
-    } else {
-      res.redirect('/login');
-    }
-  });
-  
+app.get("/dashboard", (req, res) => {
+  if (req.session.userId) {
+    res.render("dashboard");
+  } else {
+    res.redirect("/login");
+  }
+});
+
 // Route to display registration form
 app.get("/register", (req, res) => {
   res.render("register");
@@ -97,7 +97,7 @@ app.post("/login", (req, res) => {
   );
 });
 
-app.get('/my-tests', (req, res) => {
+app.get("/my-tests", (req, res) => {
   const userId = req.session.userId;
 
   const sql = `SELECT * FROM tests WHERE user_id = ?`;
@@ -109,32 +109,31 @@ app.get('/my-tests', (req, res) => {
       return;
     }
 
-    const testsWithDetails = rows.map(test => ({
+    const testsWithDetails = rows.map((test) => ({
       ...test,
       language_from_details: getLanguageDetails(test.language_from),
-      language_to_details: getLanguageDetails(test.language_to)
+      language_to_details: getLanguageDetails(test.language_to),
     }));
 
-    res.render('my-tests', { tests: testsWithDetails });
+    res.render("my-tests", { tests: testsWithDetails });
   });
 });
 
-app.get('/create-test', (req, res) => {
+app.get("/create-test", (req, res) => {
   const languages = ISO6391.getAllNames(); // Get all language names
-  res.render('create-test', { languages, getCode: ISO6391.getCode });
+  res.render("create-test", { languages, getCode: ISO6391.getCode });
 });
 
-
-app.post('/create-test', (req, res) => {
+app.post("/create-test", (req, res) => {
   // Extract test data from the request body
   const { test_name, language_from, language_to } = req.body;
   const user_id = req.session.userId;
-  console.log('Request Body:', req.body, 'User ID:', user_id);
+  console.log("Request Body:", req.body, "User ID:", user_id);
   // SQL to insert a new test into the tests table
   const sql = `INSERT INTO tests (user_id, test_name, language_from, language_to) VALUES (?, ?, ?, ?)`;
 
   // Execute the query
-  db.run(sql, [user_id, test_name, language_from, language_to], function(err) {
+  db.run(sql, [user_id, test_name, language_from, language_to], function (err) {
     if (err) {
       // Handle error
       console.error(err.message);
@@ -142,11 +141,11 @@ app.post('/create-test', (req, res) => {
       return;
     }
     // Redirect to a new page or send a success response
-    res.redirect('/my-tests'); // or res.status(200).send("Test created successfully");
+    res.redirect("/my-tests"); // or res.status(200).send("Test created successfully");
   });
 });
 
-app.post('/add-entry/:testId', (req, res) => {
+app.post("/add-entry/:testId", (req, res) => {
   const testId = req.params.testId;
   const { word_or_sentence_from, word_or_sentence_to } = req.body;
 
@@ -154,19 +153,23 @@ app.post('/add-entry/:testId', (req, res) => {
   const sql = `INSERT INTO entries (test_id, word_or_sentence_from, word_or_sentence_to) VALUES (?, ?, ?)`;
 
   // Execute the query
-  db.run(sql, [testId, word_or_sentence_from, word_or_sentence_to], function(err) {
-    if (err) {
-      // Handle error
-      console.error(err.message);
-      res.status(500).send("Error adding entry");
-      return;
+  db.run(
+    sql,
+    [testId, word_or_sentence_from, word_or_sentence_to],
+    function (err) {
+      if (err) {
+        // Handle error
+        console.error(err.message);
+        res.status(500).send("Error adding entry");
+        return;
+      }
+      // Redirect or send a success response
+      res.redirect("/edit-test/" + testId); // Redirect back to the test editing page
     }
-    // Redirect or send a success response
-    res.redirect('/edit-test/' + testId); // Redirect back to the test editing page
-  });
+  );
 });
 
-app.get('/edit-test/:testId', (req, res) => {
+app.get("/edit-test/:testId", (req, res) => {
   const testId = req.params.testId;
 
   // Fetch the test details
@@ -178,70 +181,92 @@ app.get('/edit-test/:testId', (req, res) => {
     }
 
     // Fetch the entries for this test
-    db.all("SELECT * FROM entries WHERE test_id = ?", [testId], (err, entries) => {
-      if (err) {
-        // Handle error
-        res.status(500).send("Error retrieving entries");
-        return;
+    db.all(
+      "SELECT * FROM entries WHERE test_id = ?",
+      [testId],
+      (err, entries) => {
+        if (err) {
+          // Handle error
+          res.status(500).send("Error retrieving entries");
+          return;
+        }
+
+        const languageOptions = ISO6391.getAllNames().map((name) => ({
+          name,
+          code: ISO6391.getCode(name),
+        }));
+
+        // Render the edit page, passing test details, entries, and language options
+        res.render("edit-test", {
+          test,
+          entries,
+          languageOptions,
+        });
       }
-
-      const languageOptions = ISO6391.getAllNames().map(name => ({
-        name,
-        code: ISO6391.getCode(name)
-      }));
-
-      // Render the edit page, passing test details, entries, and language options
-      res.render('edit-test', { 
-        test, 
-        entries, 
-        languageOptions 
-      });
-    });
+    );
   });
 });
 
-
-app.post('/edit-test/:testId', (req, res) => {
+app.post("/edit-test/:testId", (req, res) => {
   const testId = req.params.testId;
-  const { test_name, language_from, language_to, word_or_sentence_from, word_or_sentence_to, entry_ids } = req.body;
+  const {
+    test_name,
+    language_from,
+    language_to,
+    word_or_sentence_from,
+    word_or_sentence_to,
+    entry_ids,
+  } = req.body;
 
   // Ensure word_or_sentence_from and word_or_sentence_to are arrays
-  const wordsFrom = Array.isArray(word_or_sentence_from) ? word_or_sentence_from : [word_or_sentence_from];
-  const wordsTo = Array.isArray(word_or_sentence_to) ? word_or_sentence_to : [word_or_sentence_to];
+  const wordsFrom = Array.isArray(word_or_sentence_from)
+    ? word_or_sentence_from
+    : [word_or_sentence_from];
+  const wordsTo = Array.isArray(word_or_sentence_to)
+    ? word_or_sentence_to
+    : [word_or_sentence_to];
   const ids = Array.isArray(entry_ids) ? entry_ids : [entry_ids];
 
   // Update the test details
-  db.run("UPDATE tests SET test_name = ?, language_from = ?, language_to = ? WHERE id = ?", [test_name, language_from, language_to, testId], err => {
-    if (err) {
-      res.status(500).send("Error updating test");
-      return;
-    }
-
-    // Handle each entry
-    wordsFrom.forEach((from, index) => {
-      const to = wordsTo[index];
-      const entryId = ids[index];
-
-      if (entryId) {
-        // Update existing entry
-        db.run("UPDATE entries SET word_or_sentence_from = ?, word_or_sentence_to = ? WHERE id = ?", [from, to, entryId]);
-      } else if (from && to) {
-        // Insert new entry
-        db.run("INSERT INTO entries (test_id, word_or_sentence_from, word_or_sentence_to) VALUES (?, ?, ?)", [testId, from, to]);
+  db.run(
+    "UPDATE tests SET test_name = ?, language_from = ?, language_to = ? WHERE id = ?",
+    [test_name, language_from, language_to, testId],
+    (err) => {
+      if (err) {
+        res.status(500).send("Error updating test");
+        return;
       }
-    });
 
-    res.redirect('/my-tests');
-  });
+      // Handle each entry
+      wordsFrom.forEach((from, index) => {
+        const to = wordsTo[index];
+        const entryId = ids[index];
+
+        if (entryId) {
+          // Update existing entry
+          db.run(
+            "UPDATE entries SET word_or_sentence_from = ?, word_or_sentence_to = ? WHERE id = ?",
+            [from, to, entryId]
+          );
+        } else if (from && to) {
+          // Insert new entry
+          db.run(
+            "INSERT INTO entries (test_id, word_or_sentence_from, word_or_sentence_to) VALUES (?, ?, ?)",
+            [testId, from, to]
+          );
+        }
+      });
+
+      res.redirect("/my-tests");
+    }
+  );
 });
 
-
-
-app.post('/delete-test/:testId', (req, res) => {
+app.post("/delete-test/:testId", (req, res) => {
   const testId = req.params.testId;
 
   // Delete the entries first
-  db.run("DELETE FROM entries WHERE test_id = ?", [testId], err => {
+  db.run("DELETE FROM entries WHERE test_id = ?", [testId], (err) => {
     if (err) {
       // Handle error
       res.status(500).send("Error deleting entries");
@@ -249,22 +274,22 @@ app.post('/delete-test/:testId', (req, res) => {
     }
 
     // Now delete the test
-    db.run("DELETE FROM tests WHERE id = ?", [testId], err => {
+    db.run("DELETE FROM tests WHERE id = ?", [testId], (err) => {
       if (err) {
         // Handle error
         res.status(500).send("Error deleting test");
         return;
       }
 
-      res.redirect('/my-tests');
+      res.redirect("/my-tests");
     });
   });
 });
 
-app.post('/delete-entry/:entryId', (req, res) => {
+app.post("/delete-entry/:entryId", (req, res) => {
   const entryId = req.params.entryId;
 
-  db.run("DELETE FROM entries WHERE id = ?", [entryId], err => {
+  db.run("DELETE FROM entries WHERE id = ?", [entryId], (err) => {
     if (err) {
       // Handle error
       console.error(err.message);
@@ -273,24 +298,102 @@ app.post('/delete-entry/:entryId', (req, res) => {
     }
 
     // Send a JSON response indicating success
-    res.json({ success: true, message: "Entry deleted successfully", testId: req.body.testId });
+    res.json({
+      success: true,
+      message: "Entry deleted successfully",
+      testId: req.body.testId,
+    });
+  });
+});
+
+app.get("/run-test/:testId", (req, res) => {
+  const testId = req.params.testId;
+
+  // First, get the test details
+  db.get("SELECT * FROM tests WHERE id = ?", [testId], (testErr, test) => {
+    if (testErr) {
+      console.error(testErr.message);
+      res.status(500).send("Error retrieving test details");
+      return;
+    }
+
+    // Add language details to the test object
+    test.language_from_details = getLanguageDetails(test.language_from);
+    test.language_to_details = getLanguageDetails(test.language_to);
+
+    // Then, get all the entries for this test
+    db.all(
+      "SELECT * FROM entries WHERE test_id = ?",
+      [testId],
+      (entriesErr, entries) => {
+        if (entriesErr) {
+          console.error(entriesErr.message);
+          res.status(500).send("Error retrieving entries");
+          return;
+        }
+
+        // Shuffle entries
+        const shuffledEntries = entries.sort(() => 0.5 - Math.random());
+
+        // Render the run-test page with the test details and shuffled entries
+        res.render("run-test", { test: test, entries: shuffledEntries });
+      }
+    );
+  });
+});
+
+app.post('/submit-test/:testId', (req, res) => {
+  const testId = req.params.testId;
+  const userId = req.session.userId;
+  const userAnswers = req.body.answers; // This is an array of answers
+
+  // Fetch the correct answers in the same order as they were in the test
+  db.all("SELECT word_or_sentence_to FROM entries WHERE test_id = ? ORDER BY id", [testId], (err, correctAnswers) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Error retrieving correct answers");
+      return;
+    }
+
+    let score = 0;
+    userAnswers.forEach((answer, index) => {
+      if (index < correctAnswers.length && answer.trim().toLowerCase() === correctAnswers[index].word_or_sentence_to.trim().toLowerCase()) {
+        score++;
+      }
+    });
+
+    const totalQuestions = correctAnswers.length;
+    const percentage = (score / totalQuestions) * 100;
+
+    // Save the test results to the database
+    const insertSql = "INSERT INTO test_results (user_id, test_id, score, total_questions, percentage) VALUES (?, ?, ?, ?, ?)";
+    db.run(insertSql, [userId, testId, score, totalQuestions, percentage], insertErr => {
+      if (insertErr) {
+        console.error(insertErr.message);
+        res.status(500).send("Error saving test results");
+        return;
+      }
+
+      // Render the results page or redirect
+      res.render('test-results', {
+        score: score,
+        totalQuestions: totalQuestions,
+        percentage: percentage
+      });
+    });
   });
 });
 
 
-
-
-
-app.post('/logout', (req, res) => {
-    req.session.destroy(err => {
-      if (err) {
-        return res.redirect('/dashboard');
-      }
-      res.clearCookie('connect.sid');
-      res.redirect('/login');
-    });
+app.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.redirect("/dashboard");
+    }
+    res.clearCookie("connect.sid");
+    res.redirect("/login");
   });
-  
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
